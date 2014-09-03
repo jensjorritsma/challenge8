cb4f7298cbebb3ac9701b98c5567dc9ac0d967ae
Jens Jorritsma <jens.jorritsma@gmail.com>
2014-09-03 13:43:45 -0500

    Maybe this will work?

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,26 +2,3 @@ include_recipe 'rackops_rolebook::rack_user'
 include_recipe 'phpstack::apache'
 include_recipe 'phpstack::application_php'
 include_recipe 'phpstack::mysql_base'
-
-mysql_connection_info = {
-  :host => "localhost",
-  :username => 'root',
-  :password => node['mysql']['server_root_password']
-}
-
-mysql_database "challenge_db" do
-  connection mysql_connection_info
-  database_name node['site1']
-  action :create
-end
-
-remote_file "/root/drupal.sql" do
-  source "http://5f8981e76f355afba706-4f938954db59b7e824997e62ff19ce4f.r83.cf1.rackcdn.com/drupal.sql"
-  action :create
-  notifies :run, "execute[import]"
-end
-
-execute "import" do
-  command "/usr/bin/mysql #{node['site1']} < /root/drupal.sql"
-  action :nothing
-end
